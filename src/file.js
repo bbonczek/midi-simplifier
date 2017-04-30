1259bf41e1b023a152feb5f93c40a331e8ed0b6c
Bartosz Bączek (bartbac177) <bartbac177@student.polsl.pl>
2017-04-30 18:00:35 +0200

    file.js can open json file

--- src/file.js
+++ src/file.js
@@ -2,8 +2,15 @@ var fs = require("fs");
 
 
 const file = {
+    openJson: (path) => {
+        return new Promise((resolve, reject) => {
+            fs.readFile(path, 'utf-8', (err, data) => {
+                err ? reject(err) : resolve(data);
+            });
+        });
+    },
+    
     openMidi: (path) => {
-
         return new Promise((resolve, reject) => {
             fs.readFile(path, "binary", (err, data) => {
                 err ? reject(err) : resolve(data);
