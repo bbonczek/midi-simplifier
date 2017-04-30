3717e5e6f7ddcd7aa6718e783d014abbfcf7a384
Bartosz Bączek (bartbac177) <bartbac177@student.polsl.pl>
2017-04-30 14:15:45 +0200

    moved args handling to cmdArgs.js

---
+++ cmdArgs.js
@@ -0,0 +1,27 @@
+const cmdArgs = {
+    build: (cmdOptions) => {
+
+        if (!cmdOptions.target) {
+            let path = cmdOptions.source.split('/');
+ 
+            path[path.length - 1] = path[path.length - 1].replace('.mid', '_simple.mid');
+            cmdOptions.target = path.join('/');
+        }
+
+        if (!cmdOptions.originaljson) {
+            let path = cmdOptions.source.split('/');
+        
+            path[path.length - 1] = path[path.length - 1].replace('.mid', '_original.json');
+            cmdOptions.originaljson = path.join('/');
+        }
+
+        if (!cmdOptions.simplifiedjson) {
+            let path = cmdOptions.source.split('/');
+
+            path[path.length - 1] = path[path.length - 1].replace('.mid', '_simplified.json');
+            cmdOptions.simplifiedjson = path.join('/');
+        }
+    }
+}
+
+module.exports = cmdArgs;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var MidiConvert = require("midiconvert");
-var commandLineArgs = require('command-line-args');
 var fs = require("fs");
+var commandLineArgs = require("command-line-args");
+var cmdArgs = require("./cmdArgs.js");
 
 const optionDefinitions = [
     {name: 'source', alias: 's', type: String},
@@ -9,29 +10,12 @@ const optionDefinitions = [
     {name: 'simplifiedjson', type: String}
 ];
 
-
 const cmdOptions = commandLineArgs(optionDefinitions);
 
-if (!cmdOptions.target) {
-    let path = cmdOptions.source.split('/');
- 
-    path[path.length - 1] = path[path.length - 1].replace('.mid', '_simple.mid');
-    cmdOptions.target = path.join('/');
-}
+cmdArgs.build(cmdOptions);
 
-if (!cmdOptions.originaljson) {
-    let path = cmdOptions.source.split('/')
+console.log(cmdOptions);
 
-    path[path.length - 1] = path[path.length - 1].replace('.mid', '_original.json');
-    cmdOptions.originaljson = path.join('/');
-}
-
-if (!cmdOptions.simplifiedjson) {
-    let path = cmdOptions.source.split('/')
-
-    path[path.length - 1] = path[path.length - 1].replace('.mid', '_original.json');
-    cmdOptions.simplifiedjson = path.join('/');
-}
 
 
 // function getMidi(path) {
