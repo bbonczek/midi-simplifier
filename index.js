2c35e29a0830f624d8e5cccc1107eef5c113198d
Bartosz Bączek (bartbac177) <bartbac177@student.polsl.pl>
2017-04-30 14:03:07 +0200

    rebulding, to make it easier to use. Added command line arguments

--- index.js
+++ index.js
@@ -1,48 +1,78 @@
 var MidiConvert = require("midiconvert");
+var commandLineArgs = require('command-line-args');
 var fs = require("fs");
 
-function getMidi(path) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, "binary", (err, data) => {
-            err ? reject(err) : resolve(data);
-        });
-    });
+const optionDefinitions = [
+    {name: 'source', alias: 's', type: String},
+    {name: 'target', alias: 't', type: String},
+    {name: 'originaljson', type: String},
+    {name: 'simplifiedjson', type: String}
+];
+
+
+const cmdOptions = commandLineArgs(optionDefinitions);
+
+if (!cmdOptions.target) {
+    let path = cmdOptions.source.split('/');
+ 
+    path[path.length - 1] = path[path.length - 1].replace('.mid', '_simple.mid');
+    cmdOptions.target = path.join('/');
 }
 
-function parse(blob) {
-    let parsedMidi = MidiConvert.parse(blob);
-    return parsedMidi;
+if (!cmdOptions.originaljson) {
+    let path = cmdOptions.source.split('/')
+
+    path[path.length - 1] = path[path.length - 1].replace('.mid', '_original.json');
+    cmdOptions.originaljson = path.join('/');
 }
 
-function simplify(json) {
+if (!cmdOptions.simplifiedjson) {
+    let path = cmdOptions.source.split('/')
+
+    path[path.length - 1] = path[path.length - 1].replace('.mid', '_original.json');
+    cmdOptions.simplifiedjson = path.join('/');
+}
+
+
+// function getMidi(path) {
+//     return new Promise((resolve, reject) => {
+//         fs.readFile(path, "binary", (err, data) => {
+//             err ? reject(err) : resolve(data);
+//         });
+//     });
+// }
+
+// function parse(blob) {
+//     let parsedMidi = MidiConvert.parse(blob);
+//     return parsedMidi;
+// }
+
+// function simplify(json) {
     
-    return json;
-}
+//     return json;
+// }
 
-function saveJson(path, content) {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(path, JSON.stringify(content, null, 2), (err) => {
-            err ? reject(err) : resolve();
-        });
-    });
-}
+// function saveAsJson(path, content) {
+//     return new Promise((resolve, reject) => {
+//         fs.writeFile(path, JSON.stringify(content, null, 2), (err) => {
+//             err ? reject(err) : resolve();
+//         });
+//     });
+// }
 
+// function saveAsMidi(path, content) {
+//     return new Promise((resolve, reject) => {
+//         fs.writeFileSync(path, content.encode(), "binary");
+//     });
+// }
 
+// getMidi("samples/solo_cello/cs1-1pre.mid").then((blob) => {
+//     let midi = simplify(parse(blob));
 
-
-
-
-
-
-
-
-getMidi("samples/solo_cello/cs1-1pre.mid").then((blob) => {
-    let jsonedMidi = parse(blob);
-    let simplifiedJson = simplify(jsonedMidi);
-    saveJson("output/out.json", simplifiedJson).then(() => {
-        console.log('sved');
-    });
-}, (err) => {
-    console.log(err);
-});
+//     saveAsJson("output/out.json", midi).then(() => {
+//         saveAsMidi("gowno.mid", midi)
+//     });
+// }, (err) => {
+//     console.log(err);
+// });
 
