26462827b55b7213d268f29594bda058acfffb30
Bartosz Bączek (bartbac177) <bartbac177@student.polsl.pl>
2017-04-30 17:59:52 +0200

    chagned cmdArgs, to produce originaljson paramater only if 'toolName' flag is passed

--- src/cmdArgs.js
+++ src/cmdArgs.js
@@ -1,6 +1,6 @@
 
 const cmdArgs = {
-    build: (cmdOptions) => {
+    build: (cmdOptions, toolName) => {
 
         if (!cmdOptions.target) {
             let path = cmdOptions.source.split('/');
@@ -9,18 +9,18 @@ const cmdArgs = {
             cmdOptions.target = path.join('/');
         }
 
-        if (!cmdOptions.originaljson) {
-            let path = cmdOptions.source.split('/');
-        
-            path[path.length - 1] = path[path.length - 1].replace('.mid', '_original.json');
-            cmdOptions.originaljson = path.join('/');
-        }
-
         if (!cmdOptions.simplifiedjson) {
             let path = cmdOptions.source.split('/');
 
             path[path.length - 1] = path[path.length - 1].replace('.mid', '_simplified.json');
             cmdOptions.simplifiedjson = path.join('/');
+        }
+
+        if (toolName == "midi2midi" && !cmdOptions.originaljson) {
+            let path = cmdOptions.source.split('/');
+        
+            path[path.length - 1] = path[path.length - 1].replace('.mid', '_original.json');
+            cmdOptions.originaljson = path.join('/');
         }
     }
 }
