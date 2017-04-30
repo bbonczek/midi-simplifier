5c5c9e50a9252b67cf4f36d854909d06f227eaeb
Bartosz Bączek (bartbac177) <bartbac177@student.polsl.pl>
2017-04-30 12:13:45 +0200

    added save to file

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-console.log('hello from node');
+var MidiConvert = require("midiconvert");
+var fs = require("fs");
+
+function getMidi(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, "binary", (err, data) => {
+            err ? reject(err) : resolve(data);
+        });
+    });
+}
+
+function parse(blob) {
+    let parsedMidi = MidiConvert.parse(blob);
+    return parsedMidi;
+}
+
+function simplify(json) {
+    
+    return json;
+}
+
+function saveJson(path, content) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(path, JSON.stringify(content, null, 2), (err) => {
+            err ? reject(err) : resolve();
+        });
+    });
+}
+
+
+
+
+
+
+
+
+
+
+getMidi("samples/solo_cello/cs1-1pre.mid").then((blob) => {
+    let jsonedMidi = parse(blob);
+    let simplifiedJson = simplify(jsonedMidi);
+    saveJson("output/out.json", simplifiedJson).then(() => {
+        console.log('sved');
+    });
+}, (err) => {
+    console.log(err);
+});
+
