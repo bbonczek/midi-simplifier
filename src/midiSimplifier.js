c1ca4fde21fa317b59e61efbecfe1d9050dddb97
Bartosz Bączek (bartbac177) <bartbac177@student.polsl.pl>
2017-05-09 21:49:51 +0200

    added removing tracks, that do not contain 'notes' param

--- src/midiSimplifier.js
+++ src/midiSimplifier.js
@@ -2,10 +2,19 @@ const midiSimplifier = {
     simplify: (midi) => {
         return new Promise((resolve, reject) => {
             
-            // obviously there is no error now
             let err = null;
-            let simplifiedMidi = midi;
-            err ? reject(err) : resolve(simplifiedMidi);
+
+            // trim all tracks, that do not contain 'notes' property
+            for (i = 0; i < midi.tracks.length; i++) {
+                if (!midi.tracks[i].notes) {
+                    midi.tracks.splice(i--, 1);
+                }
+            }
+
+            
+            console.log(midi);
+
+            err ? reject(err) : resolve(midi);
         });
     }
 }
